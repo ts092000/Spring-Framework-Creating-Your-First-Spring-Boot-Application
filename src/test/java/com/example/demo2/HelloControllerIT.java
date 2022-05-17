cc2605ffac5fd775440c9b6543736204c4ec02e6
ts092000 <ts092000@gmail.com>
2022-05-17 14:36:43 +0700

    Create a spring boot application project on https://spring.io/guides/gs/spring-boot/

---
+++ src/test/java/com/example/demo2/HelloControllerIT.java
@@ -0,0 +1,21 @@
+package com.example.demo2;
+
+import org.assertj.core.api.AssertionsForClassTypes;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HelloControllerIT {
+
+    @Autowired
+    private TestRestTemplate template;
+
+    @Test
+    public void getHello() throws Exception {
+        ResponseEntity<String> response = template.getForEntity("/", String.class);
+        AssertionsForClassTypes.assertThat(response.getBody()).isEqualTo("Greetings from Spring Boot!");
+    }
+}
